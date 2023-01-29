a5e668a6fcbbe0ba613c388bf883cbd5334e26a0
Andrei_Filimonov <filimonoff.a@gmail.com>
2023-01-29 12:31:36 +0800

    Decision of task4

--- C#/Workshop1/task4/Program.cs
+++ C#/Workshop1/task4/Program.cs
@@ -2,3 +2,19 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.Clear();
+Console.Write("Введите число: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Введите еще одно число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.Write("И последнее число: ");
+int thirdNumber = int.Parse(Console.ReadLine());
+int maxNumber = firstNumber;
+if (secondNumber > maxNumber) maxNumber = secondNumber;
+if (thirdNumber > maxNumber) maxNumber = thirdNumber;
+Console.WriteLine("Максимальное из всех ваших чисел: " + maxNumber);
+// if (firstNumber > secondNumber && firstNumber > thirdNumber) Console.WriteLine("Максимальное из всех ваших чисел это " + firstNumber);
+// if (secondNumber > firstNumber && secondNumber > thirdNumber) Console.WriteLine("Максимальное из всех ваших чисел это " + secondNumber);
+// if (thirdNumber > firstNumber && thirdNumber > secondNumber) Console.WriteLine("Максимальное из всех ваших чисел это " + thirdNumber);
+// if (firstNumber == secondNumber || firstNumber == thirdNumber || secondNumber == thirdNumber) Console.WriteLine("Вводить нада разные числа. Попробуйте еще раз");
