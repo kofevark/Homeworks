3f1fa73426f310f49abfedafce148b455cfe7047
Andrei_Filimonov <filimonoff.a@gmail.com>
2023-01-30 11:11:41 +0800

    Decision of task8

--- C#/Workshop1/task8/Program.cs
+++ C#/Workshop1/task8/Program.cs
@@ -1,3 +1,26 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.Write("Введите любое число, отличающееся от 1: ");
+int N = int.Parse(Console.ReadLine());
+if (N != 1) Console.WriteLine("Вот вам все четные числа от 1 до введенного:");
+int count = 1;
+if (N > 1)
+{
+    while (count <= N)
+    {
+        if (count % 2 == 0) Console.Write(count + ", ");
+        count++;
+    }
+}
+if (N < 1)
+{
+    while (count >= N)
+    {
+        if (count % 2 == 0) Console.Write(count + ", ");
+        count--;
+    }
+}
+if (N == 1) Console.WriteLine("Говорил же не вводить 1...");
