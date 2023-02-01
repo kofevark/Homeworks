a845669c3ee282177d2ad5ebf1721790f07c848f
Andrei_Filimonov <filimonoff.a@gmail.com>
2023-02-01 10:25:42 +0800

    Decision of task15

--- C#/Workshop1/task15/Program.cs
+++ C#/Workshop1/task15/Program.cs
@@ -2,3 +2,15 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Clear();
+Console.Write("Введите день недели: ");
+int day = int.Parse(Console.ReadLine());
+if (day == 6 || day == 7)
+{
+    Console.Write("Поздравляю, сегодня");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(" выходной!");
+}
+if (day > 0 & day < 6) Console.WriteLine("Это рабочий день, но выходные уже скоро ...");
+if (day < 1 || day > 7) Console.WriteLine("В неделе СЕМЬ дней!");
