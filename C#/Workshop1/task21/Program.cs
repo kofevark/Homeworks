27e8c955900e47c7bd924d064f7c25198310c9a1
Andrei_Filimonov <filimonoff.a@gmail.com>
2023-02-06 10:07:05 +0800

    Final polishing of task21

--- C#/Workshop1/task21/Program.cs
+++ C#/Workshop1/task21/Program.cs
@@ -3,17 +3,26 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.Clear();
-Console.Write("Введите координату X для точки А: ");
+Console.WriteLine("Переместимся в трехмерное пространство.\nИ посчитаем расстояние между двумя точками.");
+
+// Организуем все в виде небольщой таблички
+Console.WriteLine("X     Y     Z".PadLeft(42)); // Сместим для центрирования
+Console.WriteLine(("-------------").PadLeft(42)); // Подчеркнем для красоты
+
+// В этом блоке для ввода координаты устанавливаем курсор в нужное место и оттуда считываем значение
+Console.Write("Введите координаты точки А:  ");
 int Ax = int.Parse(Console.ReadLine());
-Console.Write("Введите координату Y для точки А: ");
+Console.SetCursorPosition(35, 4);
 int Ay = int.Parse(Console.ReadLine());
-Console.Write("Введите координату Z для точки А: ");
+Console.SetCursorPosition(40, 4);
 int Az = int.Parse(Console.ReadLine());
-Console.Write("Введите координату X для точки B: ");
+Console.Write("Введите координаты точки B:  ");
 int Bx = int.Parse(Console.ReadLine());
-Console.Write("Введите координату Y для точки B: ");
+Console.SetCursorPosition(35, 5);
 int By = int.Parse(Console.ReadLine());
-Console.Write("Введите координату Z для точки B: ");
+Console.SetCursorPosition(40, 5);
 int Bz = int.Parse(Console.ReadLine());
-double S = Math.Sqrt(Math.Pow(Ax - Bx, 2) + Math.Pow(Ay - By, 2) + Math.Pow(Az - Bz, 2));
-Console.WriteLine($"Расстояние между точками A и B равно {S}");
+
+// Вычислим расстояние между точками и округлим полученный результат
+double S = Math.Round(Math.Sqrt(Math.Pow(Ax - Bx, 2) + Math.Pow(Ay - By, 2) + Math.Pow(Az - Bz, 2)), 2);
+Console.WriteLine($"Расстояние между точками A и B равно ~{S}");
