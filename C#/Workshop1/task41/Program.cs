0615d31f854084e602fde83c880303aa95d96fb8
Andrei_Filimonov <filimonoff.a@gmail.com>
2023-02-15 22:46:34 +0800

    Added task41 and task43

---
+++ C#/Workshop1/task41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223 -> 3
+
+Console.Write("Введите числа через пробел и нажмите Ввод: "); // Вводим числа. Можно не целые, но с запятой
+string input = Console.ReadLine()!; // Переводим введенное в строку
+string[] subs = input.Split(); // Создаем из строки строковый массив
+double[] array = new double[subs.Length]; // Глобально объявляем double массив с длиной, равной количеству введенных чисел
+ConvertStringToDouble(subs); // Ковертируем строковый массив в double
+PrintDoubleArray(array); // Печатаем массив на всякий случай
+Console.WriteLine($" -> {FindPositiveNums(array)}"); // Показываем, сколько из введенных пользователем чисел положительны
+
+// Метод преобразования массива string в double
+double[] ConvertStringToDouble(string[] someArray)
+{
+    for (int i = 0; i < subs.Length; i++) array[i] = Double.Parse(subs[i]); // Конвертируем строку в число с запятой. При вводе необходимо вводить числа через запятую (при необходимости), а не через точку
+    return array;
+}
+
+// Метод поиска положительных значений
+int FindPositiveNums(double[] someArray)
+{
+    int counter = 0;
+    foreach (double element in array) if (element > 0) counter++; // Пробегаем по всем значениям в массиве
+    return counter;
+}
+
+//Метод печати массива типа double
+void PrintDoubleArray(double[] someArray)
+{
+    // Выводим в виде [1, 2, 3, 4]
+    Console.Write("[");
+    for (int i = 0; i < someArray.Length - 1; i++) Console.Write($"{someArray[i]}, ");
+    Console.Write($"{someArray[someArray.Length - 1]}]");
+}
---
+++ C#/Workshop1/task43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+Console.WriteLine("Поищем точку пересечения двух прямых. \nПервая из которых задана уравнением y = k1 * x + b1,");
+Console.WriteLine("а вторая уравнением y = k2 * x + b2.".PadLeft(52));
+
+// Использовать метод для ввывода сообщения и ввода значения координаты в этом случае бессмысленно, т. к. кода получится гораздо больше
+Console.Write("Введите значение k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+
+FindIntersection(k1, b1, k2, b2); // Вызываем метод
+
+// Метод для нахождения и вывода в консоль точки пересечения прямых
+void FindIntersection(double x1, double y1, double x2, double y2)
+{
+    if (k1 == k2) { Console.WriteLine("Это параллельные прямые, они не пересекаются"); return; } // Проверка на параллельность
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k2 * (b2 - b1) / (k1 - k2) + b2;
+    Console.WriteLine($"Эти прямые пересекаются в точке с координатами ({x}  {y})");
+}
