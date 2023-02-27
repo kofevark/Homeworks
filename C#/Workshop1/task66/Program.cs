0ee761a13f37aca212557fc3ed83c4c41b082688
Andrei_Filimonov <filimonoff.a@gmail.com>
2023-02-27 21:35:39 +0700

    Added task64, 66 and 68

---
+++ C#/Workshop1/task66/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+
+// Работа с пользователем.
+// ---------------------
+Console.Write("Введите число M: ");
+int numberM = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine()!);
+
+// Основной процесс программы.
+// ---------------------
+Console.Write($"M = {numberM}; N = {numberN} -> ");
+
+// Проверка и переопределение переменных при необходимости.
+int temp = 0;
+if (numberM > numberN)
+{
+    temp = numberM;
+    numberM = numberN;
+    numberN = temp;
+}
+
+int sum = 0;
+sum = FindSumBetweenNumbers(numberM, numberN);
+Console.WriteLine(sum);
+
+// Методы.
+// ---------------------
+// Рекурсивный метод нахождения суммы чисел в промежутке от M до N.
+int FindSumBetweenNumbers(int m, int n)
+{
+    if (n >= m)
+    {
+        sum += n;
+        FindSumBetweenNumbers(m, n - 1);
+    }
+    return sum;
+}
---
+++ C#/Workshop1/task68/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+
+// Работа с пользователем.
+// ---------------------
+Console.WriteLine("Функция Аккермана. Осторожнее с цифрами!");
+Console.Write("Введите число m: ");
+int numberM = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число n: ");
+int numberN = int.Parse(Console.ReadLine()!);
+
+// Основной процесс программы.
+// ---------------------
+Console.WriteLine($"m = {numberM}, n = {numberN} -> A(m, n) = {AckermannFunction(numberM, numberN)}");
+
+// Методы.
+// ---------------------
+// Рекурсивный метод вычисления функции Аккермана.
+int AckermannFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return AckermannFunction(m - 1, 1);
+    }
+    return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+}
